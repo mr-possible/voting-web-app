fe60dfa358f6c3f26c58095668eb5638c7a984d0
mr-possible <sambhavd4@gmail.com>
2023-09-26 11:00:14 +0100

    Refactored login js and added styling

--- voter_service/src/components/login.js
+++ voter_service/src/components/login.js
@@ -5,50 +5,82 @@ export default function VoterLogin() {
     const [voterPassport, setVoterPassport] = useState('');
     const [voterPubKey, setVoterPubKey] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // Handle form submission (e.g., send data to the backend)
-        console.log('Voter Email Address: ', voterEmail);
-        console.log('Voter Passport No: ', voterPassport);
-        console.log('Voter Pub Key: ', voterPassport);
-    };
+        // console.log('Voter Email Address: ', voterEmail);
+        // console.log('Voter Passport No: ', voterPassport);
+        // console.log('Voter Pub Key: ', voterPubKey);
+        try {
+            // 1. Authorise the voter
+            const response = await fetch('http://127.0.0.1:8080/check-voter', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ passportNo: voterPassport, voterPublicKey: voterPubKey }),
+            });
+            if (response.ok) {
+                // 2. Show Dashboard to the User
+                console.log("Voter Authorised");
+            }
+        } catch (error) {
+            console.log('Error: ', error);
+        }
+    }
 
     return (
         <div className="voter-login-form">
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="email">Voter Email Address:</label>
-                    <input
-                        type="email"
-                        id="email"
-                        value={voterEmail}
-                        onChange={(e) => setVoterEmail(e.target.value)}
-                        required
-                    />
+            <section className="hero is-primary is-fullheight">
+                <div className="hero-body">
+                    <div className="container">
+                        <div className="columns is-centered">
+                            <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                                <form onSubmit={handleSubmit} className="box">
+                                    <div className="field">
+                                        <label htmlFor="voterEmail" className="label">Voter Email Address</label>
+                                        <div className="control has-icons-left">
+                                            <input
+                                                id="voterEmail"
+                                                type="email"
+                                                placeholder="e.g. bobsmith@gmail.com"
+                                                className="input"
+                                                onChange={(e) => setVoterEmail(e.target.value)}
+                                                required />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label htmlFor="voterPassport" className="label">Voter Passport No</label>
+                                        <div className="control has-icons-left">
+                                            <input
+                                                id="voterPassport"
+                                                type="password"
+                                                className="input"
+                                                onChange={(e) => setVoterPassport(e.target.value)}
+                                                required />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <label htmlFor="voterPubKey" className="label">Voter Public Key (Eth Address)</label>
+                                        <div className="control has-icons-left">
+                                            <input
+                                                id="voterPubKey"
+                                                type="password"
+                                                className="input"
+                                                onChange={(e) => setVoterPubKey(e.target.value)}
+                                                required />
+                                        </div>
+                                    </div>
+                                    <div className="field">
+                                        <button type="submit" className="button is-success">
+                                            Enter
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label htmlFor="passport">Voter Passport No:</label>
-                    <input
-                        type="password"
-                        id="passport"
-                        value={voterPassport}
-                        onChange={(e) => setVoterPassport(e.target.value)}
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="pubkey">Voter Public Key (Voter's Address):</label>
-                    <input
-                        type="text"
-                        id="pubkey"
-                        value={voterPubKey}
-                        onChange={(e) => setVoterPubKey(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+            </section>
         </div>
     );
 }
