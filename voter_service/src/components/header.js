239c566efd2fe2785c2ae4ecb3615386b809ff2e
mr-possible <sambhavd4@gmail.com>
2023-09-26 12:06:09 +0100

    Fix for header tag

--- voter_service/src/components/header.js
+++ voter_service/src/components/header.js
@@ -7,32 +7,35 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <nav className="navbar is-link" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a
-                    role="button"
-                    className="navbar-burger"
-                    aria-label="menu"
-                    aria-expanded="false"
-                    data-target="navbarBasicExample" >
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
-            <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
-                    <a href="#" className="navbar-item">Home</a>
-                    <a href="#" className="navbar-item">Developer Info</a>
+        <header className="header">
+            <nav className="navbar is-link" role="navigation" aria-label="main navigation">
+                <div className="navbar-brand">
+                    <a
+                        role="button"
+                        className="navbar-burger"
+                        aria-label="menu"
+                        aria-expanded="false"
+                        data-target="navbarBasicExample" >
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
                 </div>
-                <div className="navbar-end">
-                    <div className="navbar-item">
-                        <div className="buttons">
-                            <a href="#" className="button is-light">Log out</a>
+                <div id="navbarBasicExample" className="navbar-menu">
+                    <div className="navbar-start">
+                        <a href="#" className="navbar-item">Home</a>
+                        <a href="#" className="navbar-item">Developer Info</a>
+                    </div>
+                    <div className="navbar-end">
+                        <div className="navbar-item">
+                            <div className="buttons">
+                                <a href="#" className="button is-light">Log out</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </header>
+
     );
 }
