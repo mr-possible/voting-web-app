07ae19b2db122c26714168d855b5f5025dc19b43
mr-possible <sambhavd4@gmail.com>
2023-09-26 11:40:30 +0100

    Added initial styling to header

--- voter_service/src/components/header.js
+++ voter_service/src/components/header.js
@@ -1,15 +1,38 @@
+/*
+References/Credits for this file:   
+    Bulma CSS: https://bulma.io/documentation/
+*/
+
 import React from 'react';
 
 export default function Header() {
     return (
-        <header>
-            <nav>
-                <div className="logo">Ethereum Voting Service</div>
-                <ul>
-                    <li><a href="/">Dashboard</a></li>
-                    <li><a href="/login">Login</a></li>
-                </ul>
-            </nav>
-        </header>
+        <nav className="navbar is-link" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <a
+                    role="button"
+                    className="navbar-burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample" >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+            <div id="navbarBasicExample" className="navbar-menu">
+                <div className="navbar-start">
+                    <a href="#" className="navbar-item">Home</a>
+                    <a href="#" className="navbar-item">Developer Info</a>
+                </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <a href="#" className="button is-light">Log out</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
     );
 }
