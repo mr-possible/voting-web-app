b88015783070e618c6112f100f1802c86ff16efd
mr-possible <sambhavd4@gmail.com>
2023-09-26 11:22:39 +0100

    Added basic frontend for dashboard

--- voter_service/src/components/dashboard.js
+++ voter_service/src/components/dashboard.js
@@ -1,14 +1,88 @@
-import React from 'react';
+/*
+References for this file:   
+    Bulma CSS: https://bulma.io/documentation/
+*/
+
+import React, { useState } from 'react';
+import { getDataFromBlockchain, sendDataToBlockchain } from '../services/web3Client';
+import Header from './header';
 
 export default function Dashboard() {
+    const [start, setStart] = useState(false);
+
     return (
-        <div className="voter-dashboard">
-            <h2>Your Vote Matters!</h2>
-            <div className="voter-options-container">
-                <button className="voter-dashboard-button">Know Your Candidate</button>
-                <button className="voter-dashboard-button">Cast Vote</button>
-                <button className="voter-dashboard-button">Who Won ?</button>
+        // <div className="voter-dashboard">
+        //     {
+        //         !start ? (
+        //             <button onClick={() => getDataFromBlockchain("getElectionName", [])}>Test Get</button>
+        //         ) : (
+        //             <p>Hello!</p>
+        //         )}
+        //         {
+        //         !start ? (
+        //             <button onClick={() => sendDataToBlockchain("startTheElection", ["Sample"])}>Test send</button>
+        //         ) : (
+        //             <p>Hello!</p>
+        //         )}
+        // </div>
+        <>
+            <Header />
+            <div className="container is-fluid">
+                <h1 className="title is-4 has-text-centered">Welcome Voter ! What would you like to do ?</h1>
+                <div className="columns">
+                    <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="{{ asset('images/dashboard-search-book.jpg') }}" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content has-text-centered">
+                                <div className="content">
+                                    Know the candidates before casting vote!
+                                </div>
+                            </div>
+                            <footer className="card-footer">
+                                <a href="#" className="title is-4 card-footer-item">Know Candidate</a>
+                            </footer>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="{{ asset('images/dashboard-read-review.jpg') }}" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content has-text-centered">
+                                <div className="content">
+                                    Cast your vote, a single vote can make a difference!
+                                </div>
+                            </div>
+                            <footer className="card-footer has-text-centered">
+                                <a href="#" className="title is-4 card-footer-item">Cast Vote</a>
+                            </footer>
+                        </div>
+                    </div>
+                    <div className="column">
+                        <div className="card">
+                            <div className="card-image">
+                                <figure className="image is-4by3">
+                                    <img src="{{ asset('images/dashboard-submit-review.jpg') }}" alt="Placeholder image" />
+                                </figure>
+                            </div>
+                            <div className="card-content has-text-centered">
+                                <div className="content">
+                                    Get to know how did you vote turn up for the society!
+                                </div>
+                            </div>
+                            <footer className="card-footer has-text-centered">
+                                <a href="#" className="title is-4 card-footer-item">See Result</a>
+                            </footer>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
