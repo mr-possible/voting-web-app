46e0e92818e203b1838265ccd4ed6cf7e9e9aee5
mr-possible <sambhavd4@gmail.com>
2023-09-26 11:53:10 +0100

    Added basic footer

---
+++ voter_service/src/components/footer.js
@@ -0,0 +1,14 @@
+/*
+References/Credits for this file:   
+    Bulma CSS: https://bulma.io/documentation/
+*/
+
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer class="footer has-background-dark has-text-white has-text-centered">
+            <p class="has-text-grey-light">&copy; Sambhav Dave | Student Id: 2267358 | CSCM20 | Swansea University 2023</p>
+        </footer>
+    );
+}
