d2d24bacbe8bf0470999f4c73a7bb00de584397d
mr-possible <sambhavd4@gmail.com>
2023-09-26 16:20:38 +0100

    Replaced class with className

--- voter_service/src/components/footer.js
+++ voter_service/src/components/footer.js
@@ -7,8 +7,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <footer class="footer has-background-dark has-text-white has-text-centered">
-            <p class="has-text-grey-light">&copy; Sambhav Dave | Student Id: 2267358 | CSCM20 | Swansea University 2023</p>
+        <footer className="footer has-background-dark has-text-white has-text-centered">
+            <p className="has-text-grey-light">&copy; Sambhav Dave | Student Id: 2267358 | CSCM20 | Swansea University 2023</p>
         </footer>
     );
 }
